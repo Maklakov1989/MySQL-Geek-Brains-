e8291da23a66064a1ab3c5b506574296cf35b6f6
AlexeyM <100338347+Maklakov1989@users.noreply.github.com>
2022-10-14 11:32:52 +0300

    Add files via upload

---
+++ Lesson_9.sql
@@ -0,0 +1,46 @@
+-- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
+-- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+
+USE sample;
+
+DROP TABLE users;
+CREATE TABLE users (
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50),
+	bithday_date date,
+	created_at datetime,
+	updated_at datetime);
+
+INSERT INTO users (id, name, bithday_date,created_at,updated_at)
+values (1,	'Hello', '1989-01-19', now(), now());
+
+USE shop;
+
+START TRANSACTION;
+REPLACE INTO shop.users SELECT * FROM sample.users WHERE id = 1;
+DELETE FROM sample.users WHERE id = 1;
+COMMIT;
+
+-- Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу 
+-- "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	CASE 
+		WHEN CURTIME() BETWEEN '06:00:00' AND '12:00:00' THEN
+			SELECT 'Доброе утро';
+		WHEN CURTIME() BETWEEN '12:00:00' AND '18:00:00' THEN
+			SELECT 'Добрый день';
+		WHEN CURTIME() BETWEEN '18:00:00' AND '00:00:00' THEN
+			SELECT 'Добрый вечер';
+		ELSE
+			SELECT 'Доброй ночи';
+	END CASE;
+END //
+delimiter ;
+
+CALL hello();
+
